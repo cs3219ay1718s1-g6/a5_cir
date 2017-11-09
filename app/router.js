8fb40fd704cf0d72913f66a8882399cbb62d4cdd
Mai Anh Vu <me@maianhvu.com>
2017-11-09 17:08:26 +0800

    fix bugs in main router

--- app/router.js
+++ app/router.js
@@ -1,8 +1,28 @@
 const express = require('express')
 const router = express.Router()
+const neo4j = require('./components/database/neo4j')
+const { connect } = require('./components/architecture/Filter')
+const {
+    RequestParser,
+    QueryBuilder,
+    Neo4jAdapter,
+    ResponseBuilder
+} = require('./components/request-handling')
 
-router.get('/', (req, res) => {
-    res.status(200).send()
+const { session } = neo4j()
+const pipeline = connect([
+    new RequestParser(),
+    new QueryBuilder(),
+    new Neo4jAdapter(session),
+    new ResponseBuilder
+])
+
+router.get('/:module', (req, res) => {
+    pipeline(req).then(result => {
+        res.status(200).send(result)
+    }).catch(error => {
+        res.status(422).send(error)
+    })
 })
 
 module.exports = router
