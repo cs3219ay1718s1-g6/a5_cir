17b4c3bfc32eb8bdd6751dc2b292daa9404374ae
Mai Anh Vu <me@maianhvu.com>
2017-11-09 14:45:33 +0800

    add dotenv config

--- app/components/database/neo4j.js
+++ app/components/database/neo4j.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const neo4j = require('neo4j-driver').v1
 const username = process.env.NEO4J_USERNAME || 'neo4j'
-const password = process.env.NEO4J_PASSWORD || 'KoKoNut7R33'
+const password = process.env.NEO4J_PASSWORD || 'neo4j'
 const databaseUri = process.env.NEO4J_URI || 'bolt://localhost:7687'
 
 module.exports = () => {
